5e9003d82606f1add05c39742acfc171011bc088
danieldawson <ddawsonfitness@gmail.com>
2023-08-02 09:44:54 +0100

    QOL improvements

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /app
 COPY . .
 WORKDIR /app/cmd
 RUN go mod download
-RUN go build -o /onboarding
+RUN go build -o /time
 EXPOSE 8080
-CMD [ "/onboarding" ]
+CMD [ "/time" ]
--- handler/handler.go
+++ handler/handler.go
@@ -1,14 +1,22 @@
 package handler
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
 func serveCurrentTime(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().String()
 
-	w.Write([]byte(currentTime))
+	if r.Method != http.MethodGet {
+		invalidMethodMsg := fmt.Sprintf("invalid request method used: %v\n", r.Method)
+		log.Printf(invalidMethodMsg)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(invalidMethodMsg))
+		return
+	}
+	w.Write([]byte(time.Now().String()))
 }
 
 func InitHandler() {
