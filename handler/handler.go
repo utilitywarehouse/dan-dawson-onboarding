31276ce2a37a91cb0f9608ce65308b17f8c114f8
danieldawson <ddawsonfitness@gmail.com>
2023-08-07 16:42:17 +0100

    Removed println

--- handler/handler.go
+++ handler/handler.go
@@ -10,7 +10,6 @@ import (
 
 func serveCurrentTime(w http.ResponseWriter, r *http.Request) {
 	metrics.RM.Inc()
-	fmt.Println("got here")
 	if r.Method != http.MethodGet {
 		invalidMethodMsg := fmt.Sprintf("invalid request method used: %v\n", r.Method)
 		log.Print(invalidMethodMsg)
